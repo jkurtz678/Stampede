8d2f762d7129c385ffa05173f8a26ff3addc146c
Jackson Kurtz <jkurtz678@gmail.com>
2019-03-25 18:54:08 -0700

    Refactored buffaloid avoid code

--- Assets/Scripts/Buffaloid.cs
+++ Assets/Scripts/Buffaloid.cs
@@ -17,17 +17,29 @@ public class Buffaloid : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    Vector2 getAvoid()
     {
+
         Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, separation_radius);
         Debug.Log(hitColliders.Length);
-        
+
         if (hitColliders.Length > 0)
         {
             Debug.Log("object within radius");
             transform.right = hitColliders[0].gameObject.transform.position - transform.position;
-            transform.position = Vector2.MoveTowards(transform.position, hitColliders[0].gameObject.transform.position, -1 * speed * Time.deltaTime);
+            return Vector2.MoveTowards(transform.position, hitColliders[0].gameObject.transform.position, -1 * speed * Time.deltaTime);
         }
+        else
+        {
+            return transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 avoidDir = getAvoid();
+        move = avoidDir;
+        transform.position = move;
     }
 }
