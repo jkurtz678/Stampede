63774d9a0620b9da9007e12632e257359cd25c3e
Lukas Suess <lsuess97@gmail.com>
2019-04-12 13:19:10 -0700

    Changed movement of rider

--- Assets/Scripts/Player_riding.cs
+++ Assets/Scripts/Player_riding.cs
@@ -4,8 +4,59 @@ using UnityEngine;
 
 public class Player_riding : MonoBehaviour
 {
+    public float acceleration;
+    public float steering;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = -Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 speed = transform.up * (v * acceleration);
+        rb.AddForce(speed);
+
+        float direction = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up));
+        if (direction >= 0.0f)
+        {
+            rb.rotation += h * steering * (rb.velocity.magnitude / 5.0f);
+            //rb.AddTorque((h * steering) * (rb.velocity.magnitude / 10.0f));
+        }
+        else
+        {
+            rb.rotation -= h * steering * (rb.velocity.magnitude / 5.0f);
+            //rb.AddTorque((-h * steering) * (rb.velocity.magnitude / 10.0f));
+        }
+
+        Vector2 forward = new Vector2(0.0f, 0.5f);
+        float steeringRightAngle;
+        if (rb.angularVelocity > 0)
+        {
+            steeringRightAngle = -90;
+        }
+        else
+        {
+            steeringRightAngle = 90;
+        }
+
+        Vector2 rightAngleFromForward = Quaternion.AngleAxis(steeringRightAngle, Vector3.forward) * forward;
+        Debug.DrawLine((Vector3)rb.position, (Vector3)rb.GetRelativePoint(rightAngleFromForward), Color.green);
+
+        float driftForce = Vector2.Dot(rb.velocity, rb.GetRelativeVector(rightAngleFromForward.normalized));
+
+        Vector2 relativeForce = (rightAngleFromForward.normalized * -1.0f) * (driftForce * 10.0f);
 
 
+        Debug.DrawLine((Vector3)rb.position, (Vector3)rb.GetRelativePoint(relativeForce), Color.red);
+
+        rb.AddForce(rb.GetRelativeVector(relativeForce));
+    }
+    /*
     private Rigidbody2D rb;
     private Vector2 moveVelocity;
     private Vector3 m_EulerAngleVelocity;
@@ -13,6 +64,7 @@ public class Player_riding : MonoBehaviour
     public float revSpeed;
     public float boostSpeed;
     public float slowdownSpeed;
+    public float turnSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -46,10 +98,12 @@ public class Player_riding : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             rb.MoveRotation(rb.rotation - revSpeed * Time.fixedDeltaTime);
+            rb.AddForce(transform.right * turnSpeed);
         }
         else if (Input.GetKey(KeyCode.A))
         {
             rb.MoveRotation(rb.rotation + revSpeed * Time.fixedDeltaTime);
+            rb.AddForce(transform.right * -turnSpeed);
         }
     }
 
@@ -81,4 +135,5 @@ public class Player_riding : MonoBehaviour
         }
         return closest;
     }
+    */
 }
