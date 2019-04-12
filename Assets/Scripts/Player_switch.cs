6ddb6ca66d8210a20e5dfc4aa4819d4fc7b9a4ea
Lukas Suess <lsuess97@gmail.com>
2019-04-12 12:39:17 -0700

    Added player riding and added game manager to my scene

---
+++ Assets/Scripts/Player_switch.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_switch : MonoBehaviour
+{
+    private GameObject player_game_obj;
+    private GameObject closestBoid;
+    //False = standard, True = riding
+    private bool controlType = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_game_obj = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && controlType == false)
+        {
+            controlType = true;
+            closestBoid = FindClosestBoid();
+            
+            player_game_obj.transform.position = closestBoid.transform.position;
+            player_game_obj.transform.rotation = closestBoid.transform.rotation;
+            //Alter components of player and boid for force movement.
+            closestBoid.GetComponent<Buffaloid>().enabled = false;
+            closestBoid.GetComponent<Rigidbody2D>().isKinematic = true;
+            player_game_obj.GetComponent<Player>().enabled = false;
+            player_game_obj.GetComponent<Collider2D>().enabled = false;
+            player_game_obj.GetComponent<Player_riding>().enabled = true;
+            closestBoid.transform.parent = player_game_obj.transform;
+        }
+        else if(Input.GetKeyDown(KeyCode.E) && controlType == true)
+        {
+            controlType = false;
+            player_game_obj.GetComponent<Player_riding>().enabled = false;
+            player_game_obj.GetComponent<Collider2D>().enabled = true;
+            player_game_obj.GetComponent<Player>().enabled = true;
+            closestBoid.GetComponent<Buffaloid>().enabled = true;
+            closestBoid.GetComponent<Rigidbody2D>().isKinematic = false;
+        }
+    }
+
+
+    //Lukas
+    public GameObject FindClosestBoid()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Boids");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+}
